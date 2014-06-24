2c2b1686b5f660efce74f9edd8f1382e251b4886
Stefan Harenkamp <stefan.harenkamp@payintelligent.de>
2014-06-24 13:38:20 +0200

    adjust version

--- Frontend/PaymPaymentCreditcard/Bootstrap.php
+++ Frontend/PaymPaymentCreditcard/Bootstrap.php
@@ -50,7 +50,7 @@ class Shopware_Plugins_Frontend_PaymPaymentCreditcard_Bootstrap extends Shopware
      */
     public function getVersion()
     {
-        return "1.4.2";
+        return "1.4.3";
     }
 
     /**
